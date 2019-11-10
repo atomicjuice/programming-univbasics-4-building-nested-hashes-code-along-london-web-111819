9ad0d8b77c226103343f28276ec46e0e51892944
keemo <keemowatson1@gmail.com>
2019-11-10 00:24:10 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,13 +6,13 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        name: "Benvolio",
+        {name: "Benvolio",
         age: "17",
-        attitude: "worried",
+        attitude: "worried"},
 
-        name: "Mercutio",
+        {name: "Mercutio",
         age: "18",
-        attitude: "hot-headed"
+        attitude: "hot-headed"}
       ]
    },
    :capulet => {
@@ -21,13 +21,13 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        name: "Steven",
+        {name: "Steven",
         age: "30",
-        attitude: "confused",
+        attitude: "confused"},
 
-        name: "Nurse",
+        {name: "Nurse",
         age: "44",
-        attitude: "worried"
+        attitude: "worried"}
       ]
    }
 }
